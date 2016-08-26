39d5d44b000617becefc921b1a29c3542991e82a
Eric Lee <Eric Lee>
2016-08-26 18:50:39 +1000

    update 2

--- Game.js
+++ Game.js
@@ -1,15 +1,21 @@
-var readline = require('readline');
+var game = {
+	start: function() {
+		var readline = require('readline');
 
-var readlineThing = readline.createInterface({
-	input: process.stdin,
-	ouput: process.stdout
-});
+		var readlineThing = readline.createInterface({
+			input: process.stdin,
+			ouput: process.stdout
+		});
 
-readlineThing.question("What is your name", function(answer){
-	console.log('Hi there ' + answer + ", welcome to the game!")
-	readlineThing.close();
-})
-//console.log("This game works?");
+		readlineThing.question("What is your name", function(answer){
+			console.log('Hi there' + answer + ", and welcome to the game!")
+			readlineThing.close();
+		})
+	},
+	restart: function() {
+		game.start();
+	}
+}
 
 
 var Player = {
@@ -48,4 +54,44 @@ var finalBoss = {
 	}
 }
 
-console.log(finalBoss);
+function chooseClass(answer) {
+	Player.class = answer;
+	Player.skill["1"] = function() {
+				console.log("Take this");
+				finalBoss.health = finalBoss.health - 10;
+					}
+		if (Player.class == "Mage") {
+			Player.skill["2"] = function() {
+				console.log("Eat fire");
+				finalBoss.health = finalBoss.health - 20;
+			}
+			Player.skill["3"] = function() {
+			console.log("I haz more health");
+			if (Player.health > 10) {
+				Player.health = 20;
+			} else {
+				Player.health = Player.health + 10;
+			}
+		}
+	} else if (Player.class == "Warrior") {
+			Player.skill["2"] = function() {
+				console.log("Strike");
+				finalBoss.health = finalBoss.health - 10;
+			}
+			Player.skill["3"] = function() {
+				console.log("Overlord's Strike");
+				Player.health = Player.health + 10;
+			}
+	} else if (Player.class == "Thief") {
+			Player.skill["2"] = function() {
+				console.log("Back Stab");
+				finalBoss.health = finalBoss.health - 20;
+			}
+			Player.skill["3"] = function() {
+				console.log("scratch");
+				Player.health = Player.health + 10;
+			}
+		}
+}
+
+game.start();
