e1587c7b581d05d6bff19ca7ac46e982db44cd79
Eric Lee <Eric Lee>
2016-08-26 17:51:32 +1000

    update 1

--- Game.js
+++ Game.js
@@ -10,3 +10,42 @@ readlineThing.question("What is your name", function(answer){
 	readlineThing.close();
 })
 //console.log("This game works?");
+
+
+var Player = {
+	name: "Eric",
+	race: "Human",
+	class: "Mage",
+	gender: "Male",
+	health: 20,
+	skill: {
+		1: null,
+		2: null,
+		3: null
+	},
+	eat: function() {
+		if (Player.health== 19) {
+			health = 20;
+		}else if (Player.health == 20) {
+			console.log("You are on full health. Stop Eating.");
+		}else {
+			health = health + 2;
+		}
+	}
+}
+
+var finalBoss = {
+	name: "Boss",
+	health: 100,
+	skill: {
+		attack: function() {
+			console.log("Get rekt scrub, mlg 360 no scope");
+			Player.health = Player.health - 10;
+		},
+		taunt: function() {
+			console.log("go home noob");
+		}
+	}
+}
+
+console.log(finalBoss);
