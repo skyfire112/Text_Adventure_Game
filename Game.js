25f82d1499b41860ff69dbf0ba28d7d70fea5a9c
Eric Lee <Eric Lee>
2016-09-02 18:17:59 +1000

    update

--- Game.js
+++ Game.js
@@ -1,101 +1,139 @@
+var readline = require('readline');
+
+var readlineThing = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 var game = {
-	start: function() {
-		var readline = require('readline');
-
-		var readlineThing = readline.createInterface({
-			input: process.stdin,
-			ouput: process.stdout
-		});
-
-		readlineThing.question("What is your name", function(answer){
-			console.log('Hi there ' + answer + ", and welcome to the game!")
-			readlineThing.question("Choose your class! (Mage, Warrior, Thief)\n", function(answer){
-				chooseClass(answer);
-				console.log(Player);
-				readlineThing.close();
-			})
-		})
-	},
-	restart: function() {
-		game.start();
-	}
+    start: function() { 
+        initialize();
+    },
+    restart: function() {
+        game.start();
+    }
 }
 
+function initialize() {
+    readlineThing.question("What is your name? ", function(answer) {
+            Name(answer);
+            console.log('Hi there ' + answer + ", welcome to the game!")
+            readlineThing.question("Choose your class: (Mage, Warrior, Thief) ", function(answer) {
+                chooseClass(answer);
+                readlineThing.question("Choose your gender: Male, Female, genderless", function(answer) {
+                gender(answer);
+                console.log(Player)
+                readlineThing.close();
+            })
+        })
+    })
+}
 
 var Player = {
-	name: "Eric",
-	race: "Human",
-	class: "Mage",
-	gender: "Male",
-	health: 20,
-	skill: {
-		1: null,
-		2: null,
-		3: null
-	},
-	eat: function() {
-		if (Player.health== 19) {
-			health = 20;
-		}else if (Player.health == 20) {
-			console.log("You are on full health. Stop Eating.");
-		}else {
-			health = health + 2;
-		}
-	}
+    name: "Eric",
+    race: "Human",
+    class: "Mage",
+    gender: "Male",
+    health: 20,
+    skill: {
+        1: null,
+        2: null,
+        3: null
+    },
+    eat: function() {
+        if (Player.health == 19) {
+            health = 20;
+        } else if (Player.health == 20) {
+            console.log("You are on full health. Stop eating.");
+        } else {
+            health = health + 2;
+        }
+    }
 }
 
 var finalBoss = {
-	name: "Boss",
-	health: 100,
-	skill: {
-		attack: function() {
-			console.log("Get rekt scrub, mlg 360 no scope");
-			Player.health = Player.health - 10;
-		},
-		taunt: function() {
-			console.log("go home noob");
-		}
-	}
+    name: "Alan",
+    health: 100,
+    skill: {
+        attack: function() {
+            console.log("get rekt scrub, mlg 360 no scope");
+            Player.health = Player.health - 10;
+        },
+        taunt: function() {
+            console.log("go home noob");
+        }, 
+        stare: function() {
+            console.log("Look at my eyes...")
+        }
+    }
+}
+
+function Name(answer) {
+    Player.name = answer;
+}
+
+function gender(answer) {
+    Player.gender = answer;
 }
 
 function chooseClass(answer) {
-	Player.class = answer;
-	Player.skill["1"] = function() {
-				console.log("Take this");
-				finalBoss.health = finalBoss.health - 10;
-					}
-		if (Player.class == "Mage") {
-			Player.skill["2"] = function() {
-				console.log("Eat fire");
-				finalBoss.health = finalBoss.health - 20;
-			}
-			Player.skill["3"] = function() {
-			console.log("I haz more health");
-			if (Player.health > 10) {
-				Player.health = 20;
-			} else {
-				Player.health = Player.health + 10;
-			}
-		}
-	} else if (Player.class == "Warrior") {
-			Player.skill["2"] = function() {
-				console.log("Strike");
-				finalBoss.health = finalBoss.health - 10;
-			}
-			Player.skill["3"] = function() {
-				console.log("Overlord's Strike");
-				Player.health = Player.health + 10;
-			}
-	} else if (Player.class == "Thief") {
-			Player.skill["2"] = function() {
-				console.log("Back Stab");
-				finalBoss.health = finalBoss.health - 20;
-			}
-			Player.skill["3"] = function() {
-				console.log("scratch");
-				Player.health = Player.health + 10;
-			}
-		}
+    Player.class = answer;
+    Player.skill["1"] = function() {
+        console.log("Take this @#%!@#@!#!@!");
+        finalBoss.health = finalBoss.health - 10;
+    }
+    if (Player.class == "Mage") {
+        //Player.health = 10;
+        Player.skill["2"] = function() {
+            console.log("Eat fire");
+            finalBoss.health = finalBoss.health - 20;
+        }
+        Player.skill["3"] = function() {
+            console.log("I haz more health");
+            if (Player.health > 10) {
+                Player.health = 20;
+            } else {
+                Player.health = Player.health + 10;
+            }
+        }
+    } else if (Player.class == "Thief") {
+        //Player.health = 10;
+        Player.skill["2"] = function() {
+            console.log("Stabby stab stab");
+            finalBoss.health = finalBoss.health - 20;
+        }
+        Player.skill["3"] = function() {
+            console.log("Eat my arrow");
+            finalBoss.health = finalBoss.health - 15;
+        }
+    } else if (Player.class == "Warrior") {
+        //Player.health = 10;
+        Player.skill["2"] = function() {
+            console.log("Shanking intensifies");
+            finalBoss.health = finalBoss.health - 10;
+            console.log("Shanking intensifies");
+            finalBoss.health = finalBoss.health - 10;
+        }
+        Player.skill["3"] = function() {
+            console.log("This skill is a placeholder. It does nothing.");
+        }
+    } else if (Player.class == "Immortal") {
+        //Player.health = 10;
+        Player.skill["2"] = function() {
+            console.log("Earthquake");
+            finalBoss.health = finalBoss.health - 50;
+            console.log("Earthquake");
+            finalBoss.health = finalBoss.health - 50;
+        }
+        Player.skill["3"] = function() {
+            console.log("Cosmos destruction");
+            finalBoss.health = finalBoss.health - 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000;
+        }
+    }else {
+        readlineThing.question("Please enter a valid class: ", function(answer) {
+            chooseClass(answer);
+        })
+    }
 }
 
 game.start();
